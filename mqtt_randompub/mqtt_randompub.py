6a137f4c03899cc8e0be7c83d12db059c6cf59df
Fabian Affolter <mail@fabian-affolter.ch>
2013-09-06 18:22:27 +0200

    Initial import

---
+++ mqtt_randompub/mqtt_randompub.py
@@ -0,0 +1,87 @@
+# This file is part of mqtt-randompub
+#
+# Copyright (c) 2013, Fabian Affolter <fabian@affolter-engineering.ch>
+# Released under the MIT license. See LICENSE file for details.
+#
+import random
+import time
+import sys
+import os
+import itertools
+
+import mosquitto
+
+import opthandling
+import settings
+
+def send(broker, port, qos, number, interval, topic, subtopic1, subtopic2, payload):
+    count = 1
+    mqttclient = mosquitto.Mosquitto("mqtt-randompub")
+    mqttclient.connect(broker, port=int(port))
+
+    if number == 0:
+        print 'Messages are published on topic %s/#... -> CTRL + C to shutdown' \
+            % topic
+        while True:
+            complete_topic = generate_topic(topic, subtopic1, subtopic2)
+            mqttclient.publish(complete_topic, payload)
+            time.sleep(interval)
+    elif number == 1:
+        complete_topic = generate_topic(topic, subtopic1, subtopic2)
+        mqttclient.publish(complete_topic, payload)
+    else:
+        for x in range(1, number + 1):
+            complete_topic = generate_topic(topic, subtopic1, subtopic2)
+            mqttclient.publish(complete_topic, (str(count) + payload))
+            count = count + 1
+            time.sleep(interval)
+    mqttclient.disconnect()
+
+def generate_topic(topic, subtopic1, subtopic2):
+    if type(subtopic1) != list:
+        stopic1_lst = str2list(subtopic1)
+        stopic1 = random_subtopic(stopic1_lst)
+    else:
+        stopic1 = random_subtopic(subtopic1)
+    if type(subtopic2) != list:
+        stopic2_lst = str2list(subtopic2)
+        stopic2 = random_subtopic(stopic2_lst)
+    else:
+        stopic2 = random_subtopic(subtopic2)
+    generated_topic = topic + '/' + str(stopic1) + '/' + str(stopic2)
+    return generated_topic
+
+def random_subtopic(list):
+    return random.choice(list)
+
+def str2list(string):
+    str_lst = string.split(',')
+    for i, s in enumerate(str_lst):
+        str_lst[i] = s.strip()
+    return str_lst
+
+def main_run(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    args = opthandling.argparsing()
+
+    # Set a default interval of one sec if no value is given
+    interval = 1.0
+    if args.interval:
+        interval = float(args.interval)
+    
+    if args.number:
+        send(args.broker, args.port, args.qos, int(args.number), 
+            interval, args.topic, args.subtopic1, args.subtopic2, args.load)
+
+def main():
+    '''Main program entry point'''
+    try:
+        sys.exit(main_run(sys.argv))
+    except KeyboardInterrupt:
+        print 'Interrupted, exiting...'
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
---
+++ mqtt_randompub/opthandling.py
@@ -0,0 +1,68 @@
+# This file is part of mqtt-randompub
+#
+# Copyright (c) 2013 Fabian Affolter <fabian at affolter-engineering.ch>
+# Released under the MIT license. See LICENSE file for details.
+#
+import argparse
+import ConfigParser
+
+# Credits: Von Welch 
+# http://blog.vwelch.com/2011/04/combining-configparser-and-argparse.html
+def argparsing():
+    """
+    Handling command-line options, default values, and options provided by
+    confiurations files.
+    """
+    conf_parser = argparse.ArgumentParser(
+	    description = 'This tool send MQTT messages to random topics',
+        prog = 'mqtt-randompub',
+	    epilog = 'Please report all bugs and comment.',
+        formatter_class = argparse.RawDescriptionHelpFormatter,
+        add_help = False
+        )
+    conf_parser.add_argument('-c', '--config',
+                             help='configuration file to use')
+    args, remaining_argv = conf_parser.parse_known_args()
+
+    if args.config:
+        config = ConfigParser.SafeConfigParser()
+        config.read([args.config])
+        default_mqtt = dict(config.items("MQTT")) 
+        default_topic = dict(config.items("Topic"))
+        default_payload = dict(config.items("Payload"))
+    else:
+        default_mqtt = { 'broker' : '127.0.0.1',
+                         'port' : '1883',
+                         'qos' : '0'
+                        }
+        default_topic = { 'topic' : 'test',
+                          'subtopic1' : ['a', 'b', 'c'],
+                          'subtopic2' : [0, 1]
+                        }
+        default_payload = { 'load' : '#### This is a test message. ' }
+
+    parser = argparse.ArgumentParser(parents=[conf_parser])
+    parser.set_defaults(**default_mqtt)
+    parser.set_defaults(**default_topic)
+    parser.set_defaults(**default_payload)
+    parser.add_argument('-b', '--broker',
+                        help='set the broker')
+    parser.add_argument('-p', '--port',
+                        help='set the proker port')
+    parser.add_argument('-q', '--qos',
+                        help='set the QoS of the messages')
+    parser.add_argument('-t', '--topic',
+                        help='set the main topic')
+    parser.add_argument('-s', '--subtopic1',
+                        help='set the first subtopic')
+    parser.add_argument('-d', '--subtopic2',
+                        help='set the second subtopic')
+    parser.add_argument('-l', '--load',
+                        help='what to use as message payload')
+    parser.add_argument('-i', '--interval',
+                        help='time in seconds between the messages')
+    parser.add_argument('-n', '--number', default=1,
+                        help='number of messages to send. set to 0 for running ')
+
+    args = parser.parse_args(remaining_argv)
+    return args
