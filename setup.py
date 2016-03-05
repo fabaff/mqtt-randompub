89fcf3db32d5e767e3883493a8d6da234006856e
Fabian Affolter <fabian@affolter-engineering.ch>
2016-03-05 19:02:52 +0100

    Fix PEP8

--- setup.py
+++ setup.py
@@ -1,35 +1,36 @@
 # This file is part of mqtt-randompub
 #
-# Copyright (c) 2013-2014 Fabian Affolter <fabian at affolter-engineering.ch>
+# Copyright (c) 2013-2016 Fabian Affolter <fabian at affolter-engineering.ch>
 # Released under the MIT license. See LICENSE file for details.
 #
 from setuptools import setup
 import os
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 if __name__ == '__main__':
     setup(
-        name = 'mqtt-randompub',
+        name='mqtt-randompub',
         version="0.2.0",
-        description = 'Tool for generating MQTT messages on various topics',
-        long_description = read('README.rst'),
-        author = 'Fabian Affolter',
-        author_email = 'fabian@affolter-engineering.ch',
-        maintainer = 'Fabian Affolter',
-        maintainer_email = 'fabian@affolter-engineering.ch',
-        url = 'http://affolter-engineering.ch/mqtt-randompub/',
-        license = 'MIT',
-        platforms = 'Linux',
-        packages = ['mqtt_randompub'],
-        entry_points = {
+        description='Tool for generating MQTT messages on various topics',
+        long_description=read('README.rst'),
+        author='Fabian Affolter',
+        author_email='fabian@affolter-engineering.ch',
+        maintainer='Fabian Affolter',
+        maintainer_email='fabian@affolter-engineering.ch',
+        url='http://affolter-engineering.ch/mqtt-randompub/',
+        license='MIT',
+        platforms='Linux',
+        packages=['mqtt_randompub'],
+        entry_points={
             'console_scripts': ['mqtt-randompub = mqtt_randompub.mqtt_randompub:main']
         },
-        include_package_data = True,
+        include_package_data=True,
         install_requires=['paho-mqtt'],
-        keywords = ['MQTT','System','Messages'],
-        classifiers = [
+        keywords=['MQTT', 'System', 'Messages'],
+        classifiers=[
                 'Development Status :: 4 - Beta',
                 'Environment :: Console',
                 'Intended Audience :: System Administrators',
@@ -37,8 +38,8 @@ if __name__ == '__main__':
                 'License :: OSI Approved :: MIT License',
                 'Operating System :: POSIX',
                 'Programming Language :: Python',
-                'Programming Language :: Python :: 2.6',
                 'Programming Language :: Python :: 2.7',
+                'Programming Language :: Python :: 3.4',
                 'Topic :: Communications',
                 'Topic :: Internet',
                 'Topic :: System',
