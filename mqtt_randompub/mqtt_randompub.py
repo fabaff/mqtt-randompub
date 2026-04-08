8bc674f08eac8c958aebc62977908d88e9ea8be1
Fabian Affolter <mail@fabian-affolter.ch>
2026-04-08 20:39:10 +0200

    Clean-up code base (#14)
    * Remove support for paho.mqtt < 2
    
    * Update output
    
    * Add whitespace
    
    * Add typing
    
    * Make variables more readable
    
    * Remove import check

--- mqtt_randompub/mqtt_randompub.py
+++ mqtt_randompub/mqtt_randompub.py
@@ -1,46 +1,39 @@
 """Main part for sending MQTT messages."""
 
+
 import random
 import signal
 import sys
 import time
+from typing import Any, List, Optional, Union
 
 from mqtt_randompub import opthandling
 
-try:
-    import paho.mqtt.client as mqtt
-except ImportError:
-    print("Please install the paho-mqtt module to use mqtt-randompub")
+import paho.mqtt.client as mqtt
 
 
 def send(
-    broker,
-    port,
-    qos,
-    number,
-    interval,
-    topic,
-    subtopic1,
-    subtopic2,
-    payload,
-    random,
-    timestamp,
-    counter,
-):
+    broker: str,
+    port: Union[str, int],
+    qos: Union[str, int],
+    number: int,
+    interval: float,
+    topic: str,
+    subtopic1: Union[str, List[Any]],
+    subtopic2: Union[str, List[Any]],
+    payload: Union[str, List[Any], None],
+    random: bool,
+    timestamp: bool,
+    counter: bool,
+) -> None:
     """Send messages to MQTT broker."""
     count = 1
-    if "CallbackAPIVersion" in dir(mqtt):
-        # paho.mqtt 2.0+
-        mqttclient = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, "mqtt-randompub")
-    else:
-        # paho.mqtt < 2.0
-        mqttclient = mqtt.Client("mqtt-randompub")
+
+    mqttclient = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, "mqtt-randompub")
     mqttclient.connect(broker, port=int(port))
 
     if number == 0:
-        print(
-            "Messages are published on topic %s/#... ", "->CTRL + C to shutdown", topic
-        )
+        print(f"Messages are published on topic {topic}/#... -> CTRL + C to shutdown")
         while True:
             complete_topic = generate_topic(topic, subtopic1, subtopic2)
             message = generate_message(payload, timestamp, random)
@@ -64,20 +57,26 @@ def send(
                 mqttclient.publish(complete_topic, message)
             count = count + 1
             time.sleep(interval)
+    print(f"Message sent {count} messages to topic {complete_topic}")
     mqttclient.disconnect()
 
 
-def generate_message(payload, timestamp, random):
+def generate_message(
+    payload: Union[str, List[Any], None],
+    timestamp: bool,
+    random: bool
+) -> Union[str, int]:
     """The generator for the messages."""
     if random:
         generated_payload = generate_random_num()
     else:
         # Always work with a list for random_subtopic
         if isinstance(payload, list):
-            payload_lst = payload
+            payload_list = payload
         else:
-            payload_lst = str2list(payload)
-        gen_payload = random_subtopic(payload_lst)
+            payload_list = string2list(payload)
+        gen_payload = random_subtopic(payload_list)
+
         if timestamp:
             generated_payload = f"{gen_payload} {generate_timestamp()}"
         else:
@@ -85,52 +84,55 @@ def generate_message(payload, timestamp, random):
     return generated_payload
 
 
-def generate_topic(topic, subtopic1, subtopic2):
+def generate_topic(
+    topic: str,
+    subtopic1: Union[str, List[Any]],
+    subtopic2: Union[str, List[Any]]
+) -> str:
     """The generator for the topic."""
     # Always work with a list for random_subtopic
     if isinstance(subtopic1, list):
-        stopic1_lst = subtopic1
+        subtopic1_list = subtopic1
     else:
-        stopic1_lst = str2list(subtopic1)
-    stopic1 = random_subtopic(stopic1_lst)
+        subtopic1_list = string2list(subtopic1)
+    subtopic1 = random_subtopic(subtopic1_list)
 
     if isinstance(subtopic2, list):
-        stopic2_lst = subtopic2
+        subtopic2_list = subtopic2
     else:
-        stopic2_lst = str2list(subtopic2)
-    stopic2 = random_subtopic(stopic2_lst)
-
-    generated_topic = f"{topic}/{stopic1}/{stopic2}"
+        subtopic2_list = string2list(subtopic2)
+    subtopic2 = random_subtopic(subtopic2_list)
+    
+    generated_topic = f"{topic}/{subtopic1}/{subtopic2}"
     return generated_topic
 
 
-
-def random_subtopic(items):
+def random_subtopic(items: List[Any]) -> Any:
     """Return a random item from a list."""
     return random.choice(items)
 
 
-def str2list(val):
+def string2list(value: Union[str, List[Any]]) -> List[str]:
     """Return a list of strings, or the input if already a list."""
-    if isinstance(val, list):
-        return val
-    str_lst = val.split(",")
-    return [s.strip() for s in str_lst]
+    if isinstance(value, list):
+        return value
+    string_list = value.split(",")
+    return [string.strip() for string in string_list]
 
 
-def generate_random_num():
+def generate_random_num() -> int:
     """Return a random generated number."""
     return random.randrange(0, 100, 1)
 
 
-def generate_timestamp():
+def generate_timestamp() -> int:
     """Return the current timestamp."""
     timestamp = int(time.time())
     return timestamp
 
 
-def main(argv=None):
-    """Main"""
+def main(argv: Optional[List[str]] = None) -> None:
+    """Main function for mqtt-randompub."""
     if argv is None:
         argv = sys.argv
     args = opthandling.argparsing()
