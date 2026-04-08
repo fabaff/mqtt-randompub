31005dd0f67640419e2f492ae7d425e0b7a0dce3
Fabian Affolter <mail@fabian-affolter.ch>
2026-04-08 20:11:30 +0200

    Add whitespace

--- mqtt_randompub/mqtt_randompub.py
+++ mqtt_randompub/mqtt_randompub.py
@@ -34,7 +34,7 @@ def send(
     mqttclient.connect(broker, port=int(port))
 
     if number == 0:
-        print(f"Messages are published on topic {topic}/#... ->CTRL + C to shutdown")
+        print(f"Messages are published on topic {topic}/#... -> CTRL + C to shutdown")
         while True:
             complete_topic = generate_topic(topic, subtopic1, subtopic2)
             message = generate_message(payload, timestamp, random)
